2574d9769f6a3a8af7e4d342c82809f44970ed3f
Nathan Faucett <nathanfaucett@gmail.com>
2025-05-04 11:35:59 -0400

    update packages

--- src/Option.test.ts
+++ src/Option.test.ts
@@ -1,183 +1,191 @@
-import * as tape from "tape";
+import tape from "tape";
 import { none, Option, some } from ".";
 
 tape("Option", (assert: tape.Test) => {
-  assert.equal(Option.some(1).isSome(), true);
-  assert.equal(some(1).isNone(), false);
-  assert.deepEqual(Option.fromJSON(1), some(1));
+	assert.equal(Option.some(1).isSome(), true);
+	assert.equal(some(1).isNone(), false);
+	assert.deepEqual(Option.fromJSON(1), some(1));
 
-  assert.equal(Option.none().isSome(), false);
-  assert.equal(none().isNone(), true);
-  assert.deepEqual(Option.fromJSON(null), none());
+	assert.equal(Option.none().isSome(), false);
+	assert.equal(none().isNone(), true);
+	assert.deepEqual(Option.fromJSON(null), none());
 
-  assert.equal(some(1).unwrap(), 1);
-  assert.equal(
-    some(1)
-      .map((x) => x + 1)
-      .unwrap(),
-    2
-  );
-  assert.equal(
-    some(1)
-      .mapOr((x) => x + 1, 1)
-      .unwrap(),
-    2
-  );
-  assert.equal(
-    none<number>()
-      .mapOr((x) => x + 1, 1)
-      .unwrap(),
-    1
-  );
-  assert.equal(
-    some(1)
-      .mapOrElse(
-        (x) => x + 1,
-        () => 1
-      )
-      .unwrap(),
-    2
-  );
-  assert.equal(
-    none<number>()
-      .mapOrElse(
-        (x) => x + 1,
-        () => 1
-      )
-      .unwrap(),
-    1
-  );
-  assert.equal(
-    none<number>()
-      .map((x) => x + 1)
-      .isNone(),
-    true
-  );
+	assert.equal(some(1).unwrap(), 1);
+	assert.equal(
+		some(1)
+			.map((x) => x + 1)
+			.unwrap(),
+		2,
+	);
+	assert.equal(
+		some(1)
+			.mapOr((x) => x + 1, 1)
+			.unwrap(),
+		2,
+	);
+	assert.equal(
+		none<number>()
+			.mapOr((x) => x + 1, 1)
+			.unwrap(),
+		1,
+	);
+	assert.equal(
+		some(1)
+			.mapOrElse(
+				(x) => x + 1,
+				() => 1,
+			)
+			.unwrap(),
+		2,
+	);
+	assert.equal(
+		none<number>()
+			.mapOrElse(
+				(x) => x + 1,
+				() => 1,
+			)
+			.unwrap(),
+		1,
+	);
+	assert.equal(
+		none<number>()
+			.map((x) => x + 1)
+			.isNone(),
+		true,
+	);
 
-  assert.deepEqual(some(1).flatMap(some), some(1));
-  assert.deepEqual(none<number>().flatMapOr(some, some(1)), some(1));
-  assert.deepEqual(
-    none<number>().flatMapOrElse(some, () => some(1)),
-    some(1)
-  );
+	assert.deepEqual(some(1).flatMap(some), some(1));
+	assert.deepEqual(none<number>().flatMapOr(some, some(1)), some(1));
+	assert.deepEqual(
+		none<number>().flatMapOrElse(some, () => some(1)),
+		some(1),
+	);
 
-  assert.deepEqual(some(1).and(some(2)), some(2));
-  assert.deepEqual(none<number>().and(some(1)), none<number>());
+	assert.deepEqual(some(1).and(some(2)), some(2));
+	assert.deepEqual(none<number>().and(some(1)), none<number>());
 
-  assert.deepEqual(
-    some(1).andThen(() => some(2)),
-    some(2)
-  );
-  assert.deepEqual(
-    none().andThen(() => some(1)),
-    none()
-  );
+	assert.deepEqual(
+		some(1).andThen(() => some(2)),
+		some(2),
+	);
+	assert.deepEqual(
+		none().andThen(() => some(1)),
+		none(),
+	);
 
-  assert.deepEqual(some(1).or(some(2)), some(1));
-  assert.deepEqual(none().or(some(1)), some(1));
+	assert.deepEqual(some(1).or(some(2)), some(1));
+	assert.deepEqual(none().or(some(1)), some(1));
 
-  assert.deepEqual(
-    some(1).orElse(() => some(2)),
-    some(1)
-  );
-  assert.deepEqual(
-    none().orElse(() => some(1)),
-    some(1)
-  );
+	assert.deepEqual(
+		some(1).orElse(() => some(2)),
+		some(1),
+	);
+	assert.deepEqual(
+		none().orElse(() => some(1)),
+		some(1),
+	);
 
-  assert.deepEqual(some(1).xor(some(2)), none());
-  assert.deepEqual(none().xor(some(2)), some(2));
-  assert.deepEqual(some(1).xor(none()), some(1));
+	assert.deepEqual(some(1).xor(some(2)), none());
+	assert.deepEqual(none().xor(some(2)), some(2));
+	assert.deepEqual(some(1).xor(none()), some(1));
 
-  assert.deepEqual(
-    some(1).filter((x) => x === 1),
-    some(1)
-  );
-  assert.deepEqual(
-    some(2).filter((x) => x === 1),
-    none()
-  );
-  assert.deepEqual(
-    none().filter(() => true),
-    none()
-  );
+	assert.deepEqual(
+		some(1).filter((x) => x === 1),
+		some(1),
+	);
+	assert.deepEqual(
+		some(2).filter((x) => x === 1),
+		none(),
+	);
+	assert.deepEqual(
+		none().filter(() => true),
+		none(),
+	);
 
-  assert.equal(
-    none().unwrapOrElse(() => 1),
-    1
-  );
+	assert.equal(
+		none().unwrapOrElse(() => 1),
+		1,
+	);
 
-  assert.deepEqual(some(2).getOrInsert(1), some(2));
-  assert.deepEqual(none().getOrInsert(1), some(1));
-  assert.deepEqual(
-    some(2).getOrInsertWith(() => 1),
-    some(2)
-  );
-  assert.deepEqual(
-    none().getOrInsertWith(() => 1),
-    some(1)
-  );
+	assert.deepEqual(some(2).getOrInsert(1), some(2));
+	assert.deepEqual(none().getOrInsert(1), some(1));
+	assert.deepEqual(
+		some(2).getOrInsertWith(() => 1),
+		some(2),
+	);
+	assert.deepEqual(
+		none().getOrInsertWith(() => 1),
+		some(1),
+	);
 
-  const someValue = some(1);
-  assert.deepEqual(someValue.take(), some(1));
-  assert.deepEqual(someValue, none());
+	const someValue = some(1);
+	assert.deepEqual(someValue.take(), some(1));
+	assert.deepEqual(someValue, none());
 
-  assert.deepEqual(some(1).from(1), some(1));
-  assert.deepEqual(none().from(1), some(1));
-  assert.deepEqual(some(1).from(null), none());
-  assert.deepEqual(none().from(undefined), none());
+	assert.deepEqual(some(1).from(1), some(1));
+	assert.deepEqual(none().from(1), some(1));
+	assert.deepEqual(some(1).from(null), none());
+	assert.deepEqual(none().from(undefined), none());
 
-  const noneValue = none();
-  assert.deepEqual(noneValue.take(), none());
-  assert.deepEqual(noneValue, none());
+	const noneValue = none();
+	assert.deepEqual(noneValue.take(), none());
+	assert.deepEqual(noneValue, none());
 
-  assert.deepEqual(some(0).replace(1), some(1));
-  assert.deepEqual(none().replace(1), some(1));
+	assert.deepEqual(some(0).replace(1), some(1));
+	assert.deepEqual(none().replace(1), some(1));
 
-  assert.deepEqual(some({}).toJS(), {});
-  assert.deepEqual(none().toJS(), null);
-  assert.deepEqual(some({}).toJSON(), {});
-  assert.deepEqual(none().toJSON(), null);
+	assert.deepEqual(some({}).toJS(), {});
+	assert.deepEqual(none().toJS(), null);
+	assert.deepEqual(some({}).toJSON(), {});
+	assert.deepEqual(none().toJSON(), null);
 
-  let ifSomeValue = 0;
-  some(1).ifSome((x) => (ifSomeValue = x));
-  assert.equal(ifSomeValue, 1);
+	let ifSomeValue = 0;
+	some(1).ifSome((x) => {
+		ifSomeValue = x;
+	});
+	assert.equal(ifSomeValue, 1);
 
-  let ifElseCalled = false;
-  none().ifSome(
-    () => undefined,
-    () => (ifElseCalled = true)
-  );
-  assert.equal(ifElseCalled, true);
+	let ifElseCalled = false;
+	none().ifSome(
+		() => undefined,
+		() => {
+			ifElseCalled = true;
+		},
+	);
+	assert.equal(ifElseCalled, true);
 
-  let ifNoneCalled = false;
-  none().ifNone(() => (ifNoneCalled = true));
-  assert.equal(ifNoneCalled, true);
+	let ifNoneCalled = false;
+	none().ifNone(() => {
+		ifNoneCalled = true;
+	});
+	assert.equal(ifNoneCalled, true);
 
-  let ifNoneElseValue = 0;
-  some(1).ifNone(
-    () => undefined,
-    (x) => (ifNoneElseValue = x)
-  );
-  assert.equal(ifNoneElseValue, 1);
+	let ifNoneElseValue = 0;
+	some(1).ifNone(
+		() => undefined,
+		(x) => {
+			ifNoneElseValue = x;
+		},
+	);
+	assert.equal(ifNoneElseValue, 1);
 
-  assert.throws(() => {
-    none().unwrap();
-  }, /Tried to unwrap value of none Option/);
+	assert.throws(() => {
+		none().unwrap();
+	}, /Tried to unwrap value of none Option/);
 
-  assert.throws(
-    () => new Option({}, {}),
-    /Options can only be created with the some or none functions/
-  );
+	assert.throws(
+		() => new Option({}, {}),
+		/Options can only be created with the some or none functions/,
+	);
 
-  for (const value of some("test")) {
-    assert.equal(value, "test", "should loop over some option");
-  }
-  let loopedOverNone = false;
-  for (const _ of none()) {
-    loopedOverNone = true;
-  }
-  assert.equal(loopedOverNone, false, "should not loop over none option");
+	for (const value of some("test")) {
+		assert.equal(value, "test", "should loop over some option");
+	}
+	let loopedOverNone = false;
+	for (const _ of none()) {
+		loopedOverNone = true;
+	}
+	assert.equal(loopedOverNone, false, "should not loop over none option");
 
-  assert.end();
+	assert.end();
 });
--- src/Option.ts
+++ src/Option.ts
@@ -1,260 +1,244 @@
 type ICREATE_SECRET = unknown;
 type INULL_SECRET = unknown;
 
-const CREATE_SECRET: ICREATE_SECRET = {},
-  NULL_SECRET: INULL_SECRET = {};
+const CREATE_SECRET: ICREATE_SECRET = {};
+const NULL_SECRET: INULL_SECRET = {};
 
 export class Option<T> implements Iterable<T> {
-  static some<T>(value: T): Option<T> {
-    return some(value);
-  }
-  static none<T>(): Option<T> {
-    return none();
-  }
+	static some<T>(value: T): Option<T> {
+		return some(value);
+	}
+	static none<T>(): Option<T> {
+		return none();
+	}
 
-  static from<T>(value?: T | null): Option<T> {
-    if (value != null) {
-      return some(value);
-    } else {
-      return none();
-    }
-  }
+	static from<T>(value?: T | null): Option<T> {
+		if (value != null) {
+			return some(value);
+		}
+		return none();
+	}
 
-  static fromJSON<T>(json: T | null): Option<T> {
-    return Option.from(json);
-  }
+	static fromJSON<T>(json: T | null): Option<T> {
+		return Option.from(json);
+	}
 
-  private _value: T | INULL_SECRET;
+	private _value: T | INULL_SECRET;
 
-  constructor(createSecret: ICREATE_SECRET, value: T | INULL_SECRET) {
-    if (createSecret !== CREATE_SECRET) {
-      throw new TypeError(
-        "Options can only be created with the some or none functions"
-      );
-    }
-    this._value = value;
-  }
+	constructor(createSecret: ICREATE_SECRET, value: T | INULL_SECRET) {
+		if (createSecret !== CREATE_SECRET) {
+			throw new TypeError(
+				"Options can only be created with the some or none functions",
+			);
+		}
+		this._value = value;
+	}
 
-  isNone(): boolean {
-    return this._value === NULL_SECRET;
-  }
+	isNone(): boolean {
+		return this._value === NULL_SECRET;
+	}
 
-  isSome(): boolean {
-    return !this.isNone();
-  }
+	isSome(): boolean {
+		return !this.isNone();
+	}
 
-  expect(msg: (() => string) | string): T {
-    if (this.isSome()) {
-      return this._value as T;
-    } else {
-      throw new Error(typeof msg === "function" ? msg() : msg);
-    }
-  }
+	expect(msg: (() => string) | string): T {
+		if (this.isSome()) {
+			return this._value as T;
+		}
+		throw new Error(typeof msg === "function" ? msg() : msg);
+	}
 
-  unwrap(): T {
-    return this.expect("Tried to unwrap value of none Option");
-  }
-  unwrapOr(def: T): T {
-    if (this.isSome()) {
-      return this._value as T;
-    } else {
-      return def;
-    }
-  }
-  unwrapOrElse(defFn: () => T): T {
-    if (this.isSome()) {
-      return this._value as T;
-    } else {
-      return defFn();
-    }
-  }
+	unwrap(): T {
+		return this.expect("Tried to unwrap value of none Option");
+	}
+	unwrapOr(def: T): T {
+		if (this.isSome()) {
+			return this._value as T;
+		}
+		return def;
+	}
+	unwrapOrElse(defFn: () => T): T {
+		if (this.isSome()) {
+			return this._value as T;
+		}
+		return defFn();
+	}
 
-  map<U>(fn: (value: T) => U): Option<U> {
-    if (this.isSome()) {
-      return some(fn(this._value as T));
-    } else {
-      return none();
-    }
-  }
-  mapOr<U>(fn: (value: T) => U, def: U): Option<U> {
-    if (this.isSome()) {
-      return some(fn(this._value as T));
-    } else {
-      return some(def);
-    }
-  }
-  mapOrElse<U>(fn: (value: T) => U, defFn: () => U): Option<U> {
-    if (this.isSome()) {
-      return some(fn(this._value as T));
-    } else {
-      return some(defFn());
-    }
-  }
+	map<U>(fn: (value: T) => U): Option<U> {
+		if (this.isSome()) {
+			return some(fn(this._value as T));
+		}
+		return none();
+	}
+	mapOr<U>(fn: (value: T) => U, def: U): Option<U> {
+		if (this.isSome()) {
+			return some(fn(this._value as T));
+		}
+		return some(def);
+	}
+	mapOrElse<U>(fn: (value: T) => U, defFn: () => U): Option<U> {
+		if (this.isSome()) {
+			return some(fn(this._value as T));
+		}
+		return some(defFn());
+	}
 
-  flatMap<U>(fn: (value: T) => Option<U>): Option<U> {
-    if (this.isSome()) {
-      return fn(this._value as T);
-    } else {
-      return none();
-    }
-  }
-  flatMapOr<U>(fn: (value: T) => Option<U>, def: Option<U>): Option<U> {
-    if (this.isSome()) {
-      return fn(this._value as T);
-    } else {
-      return def;
-    }
-  }
-  flatMapOrElse<U>(
-    fn: (value: T) => Option<U>,
-    defFn: () => Option<U>
-  ): Option<U> {
-    if (this.isSome()) {
-      return fn(this._value as T);
-    } else {
-      return defFn();
-    }
-  }
+	flatMap<U>(fn: (value: T) => Option<U>): Option<U> {
+		if (this.isSome()) {
+			return fn(this._value as T);
+		}
+		return none();
+	}
+	flatMapOr<U>(fn: (value: T) => Option<U>, def: Option<U>): Option<U> {
+		if (this.isSome()) {
+			return fn(this._value as T);
+		}
+		return def;
+	}
+	flatMapOrElse<U>(
+		fn: (value: T) => Option<U>,
+		defFn: () => Option<U>,
+	): Option<U> {
+		if (this.isSome()) {
+			return fn(this._value as T);
+		}
+		return defFn();
+	}
 
-  and<U>(value: Option<U>): Option<U> {
-    if (this.isSome()) {
-      return value;
-    } else {
-      return none();
-    }
-  }
-  andThen<U>(fn: (value: T) => Option<U>): Option<U> {
-    if (this.isSome()) {
-      return fn(this._value as T);
-    } else {
-      return none();
-    }
-  }
+	and<U>(value: Option<U>): Option<U> {
+		if (this.isSome()) {
+			return value;
+		}
+		return none();
+	}
+	andThen<U>(fn: (value: T) => Option<U>): Option<U> {
+		if (this.isSome()) {
+			return fn(this._value as T);
+		}
+		return none();
+	}
 
-  or(value: Option<T>): Option<T> {
-    if (this.isNone()) {
-      return value;
-    } else {
-      return this;
-    }
-  }
-  orElse(fn: () => Option<T>): Option<T> {
-    if (this.isNone()) {
-      return fn();
-    } else {
-      return this;
-    }
-  }
+	or(value: Option<T>): Option<T> {
+		if (this.isNone()) {
+			return value;
+		}
+		return this;
+	}
+	orElse(fn: () => Option<T>): Option<T> {
+		if (this.isNone()) {
+			return fn();
+		}
+		return this;
+	}
 
-  xor(value: Option<T>): Option<T> {
-    const a = this.isSome(),
-      b = value.isSome();
+	xor(value: Option<T>): Option<T> {
+		const a = this.isSome();
+		const b = value.isSome();
 
-    if (a && !b) {
-      return this;
-    } else if (!a && b) {
-      return value;
-    } else {
-      return none();
-    }
-  }
+		if (a && !b) {
+			return this;
+		}
+		if (!a && b) {
+			return value;
+		}
+		return none();
+	}
 
-  filter<S extends T>(fn: (value: T) => value is S): Option<S>;
-  filter(fn: (value: T) => boolean): Option<T>;
-  filter(fn: any): any {
-    if (this.isSome() && fn(this._value)) {
-      return this;
-    } else {
-      return none();
-    }
-  }
+	filter<S extends T>(fn: (value: T) => value is S): Option<S>;
+	filter(fn: (value: T) => boolean): Option<T>;
+	// biome-ignore lint/suspicious/noExplicitAny: overwrite
+	filter(fn: any): any {
+		if (this.isSome() && fn(this._value)) {
+			return this;
+		}
+		return none();
+	}
 
-  getOrInsert(value: T): Option<T> {
-    if (this.isNone()) {
-      this._value = value;
-    }
-    return this;
-  }
-  getOrInsertWith(fn: () => T): Option<T> {
-    if (this.isNone()) {
-      this._value = fn();
-    }
-    return this;
-  }
+	getOrInsert(value: T): Option<T> {
+		if (this.isNone()) {
+			this._value = value;
+		}
+		return this;
+	}
+	getOrInsertWith(fn: () => T): Option<T> {
+		if (this.isNone()) {
+			this._value = fn();
+		}
+		return this;
+	}
 
-  clear(): Option<T> {
-    this._value = NULL_SECRET as any;
-    return this;
-  }
-  take(): Option<T> {
-    if (this.isSome()) {
-      const value = this._value;
-      this._value = NULL_SECRET as any;
-      return some(value as T);
-    } else {
-      return none();
-    }
-  }
-  from(value?: T | null): Option<T> {
-    if (value != null) {
-      this._value = value;
-    } else {
-      this.clear();
-    }
-    return this;
-  }
-  replace(value: T): Option<T> {
-    this._value = value;
-    return this;
-  }
+	clear(): Option<T> {
+		this._value = NULL_SECRET as never;
+		return this;
+	}
+	take(): Option<T> {
+		if (this.isSome()) {
+			const value = this._value;
+			this._value = NULL_SECRET as never;
+			return some(value as T);
+		}
+		return none();
+	}
+	from(value?: T | null): Option<T> {
+		if (value != null) {
+			this._value = value;
+		} else {
+			this.clear();
+		}
+		return this;
+	}
+	replace(value: T): Option<T> {
+		this._value = value;
+		return this;
+	}
 
-  ifSome(fn: (value: T) => void, elseFn?: () => void): Option<T> {
-    if (this.isSome()) {
-      fn(this._value as T);
-    } else if (elseFn) {
-      elseFn();
-    }
-    return this;
-  }
-  ifNone(fn: () => void, elseFn?: (value: T) => void): Option<T> {
-    if (this.isNone()) {
-      fn();
-    } else if (elseFn) {
-      elseFn(this._value as T);
-    }
-    return this;
-  }
+	ifSome(fn: (value: T) => void, elseFn?: () => void): Option<T> {
+		if (this.isSome()) {
+			fn(this._value as T);
+		} else if (elseFn) {
+			elseFn();
+		}
+		return this;
+	}
+	ifNone(fn: () => void, elseFn?: (value: T) => void): Option<T> {
+		if (this.isNone()) {
+			fn();
+		} else if (elseFn) {
+			elseFn(this._value as T);
+		}
+		return this;
+	}
 
-  toJSON(): T | null {
-    return this.unwrapOr(null as any);
-  }
-  toJS(): T | null {
-    return this.unwrapOr(null as any);
-  }
+	toJSON(): T | null {
+		return this.unwrapOr(null as never);
+	}
+	toJS(): T | null {
+		return this.unwrapOr(null as never);
+	}
 
-  [Symbol.iterator](): OptionIterator<T> {
-    return new OptionIterator(this._value);
-  }
+	[Symbol.iterator](): OptionIterator<T> {
+		return new OptionIterator(this._value);
+	}
 }
 
 class OptionIterator<T> implements Iterator<T> {
-  private value: T | INULL_SECRET;
+	private value: T | INULL_SECRET;
 
-  constructor(value: T | INULL_SECRET) {
-    this.value = value;
-  }
+	constructor(value: T | INULL_SECRET) {
+		this.value = value;
+	}
 
-  next(): IteratorResult<T, undefined> {
-    if (this.value === NULL_SECRET) {
-      return { done: true, value: undefined };
-    } else {
-      const value = this.value as T;
-      this.value = NULL_SECRET;
-      return { done: false, value };
-    }
-  }
+	next(): IteratorResult<T, undefined> {
+		if (this.value === NULL_SECRET) {
+			return { done: true, value: undefined };
+		}
+		const value = this.value as T;
+		this.value = NULL_SECRET;
+		return { done: false, value };
+	}
 }
 
 export const some = <T>(value: T): Option<T> =>
-  new Option(CREATE_SECRET, value);
+	new Option(CREATE_SECRET, value);
 export const none = <T>(): Option<T> => new Option(CREATE_SECRET, NULL_SECRET);
